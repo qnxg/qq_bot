44bcd943b959e309f0c067053a4b5597193ae096
oi <246788426+oi-itsme@users.noreply.github.com>
2026-08-29 21:37:22 +0800

    chore: fmt

--- libs/data-client/src/rabbitmq.rs
+++ libs/data-client/src/rabbitmq.rs
@@ -8,19 +8,18 @@ static RABBIT_CHANNEL: OnceCell<Channel> = OnceCell::const_new();
 pub async fn get_channel() -> Channel {
     RABBIT_CHANNEL
         .get_or_init(|| async {
-            let conn =
-                match Connection::connect(&CFG.rabbitmq.url, ConnectionProperties::default())
-                    .await
-                {
-                    Ok(conn) => {
-                        tracing::info!("🔥 Successfully connected to RabbitMQ");
-                        conn
-                    }
-                    Err(e) => {
-                        tracing::error!("🪨 Failed to connect to RabbitMQ: {:?}", e);
-                        std::process::exit(1);
-                    }
-                };
+            let conn = match Connection::connect(&CFG.rabbitmq.url, ConnectionProperties::default())
+                .await
+            {
+                Ok(conn) => {
+                    tracing::info!("🔥 Successfully connected to RabbitMQ");
+                    conn
+                }
+                Err(e) => {
+                    tracing::error!("🪨 Failed to connect to RabbitMQ: {:?}", e);
+                    std::process::exit(1);
+                }
+            };
             match conn.create_channel().await {
                 Ok(channel) => {
                     tracing::info!("🔥 Successfully create RabbitMQ channel");
